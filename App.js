19bf768d00ceac536b56e2420bcc947a10d47b31
fzx2666 <46859529+fzx2666@users.noreply.github.com>
2020-04-23 21:10:59 -0700

    Add files via upload

--- App.js
+++ App.js
@@ -1,19 +1,41 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <MapView
+          provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+          style={styles.map}
+          initialRegion={{
+            latitude: 42,
+            longitude: 40,
+            latitudeDelta: 90, // Pretty good swath to see a few countries
+            longitudeDelta: 180,
+          }}
+        ></MapView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  googleMap: {
+    //...StyleSheet.absoluteFillObject,
+    height: 400,
+    width: 400,
+    justifyContent: "flex-end",
+    alignItems: "center",
   },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  image: {
+    height: 64, 
+    width: 64,
+    padding:20,
+    margin:10,
+    borderRadius: 8,
+    },
 });
